e6f3db803f7dc9fa3dde566ca3e4db48a381c1ac
onyxvail <ons.hfi@gmail.com>
2023-02-17 10:37:15 -0800

    Task-0 fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,28 +3,28 @@
 #include <time.h>
 
 /**
- * main - print whether the number stored in the variable n is positive or negative
- *
- * Return: 0 (Success)
- */
+*main - Entry point
+*Description: print whether the number stored
+*in the variable n is positive or negative
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
