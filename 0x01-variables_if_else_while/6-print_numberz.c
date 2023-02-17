968c155dfb327021208cf91cc2ba07b1ec1ec5a7
onyxvail <ons.hfi@gmail.com>
2023-02-17 09:35:34 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - prints all single digit numbers
+*of base 10 starting from 0
+*followed by a new line
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int b;
+for (b = 0; b < 10; b++)
+{
+putchar(b + '0');
+}
+putchar('\n');
+return (0);
+}
