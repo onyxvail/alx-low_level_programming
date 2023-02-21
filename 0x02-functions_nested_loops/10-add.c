64bb38fdeff81a3b0507bbb69233a075f4293343
onyxvail <ons.hfi@gmail.com>
2023-02-21 12:53:22 -0800

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two int
+ * @num1: the first int to add
+ * @num2: the second int to  add
+ * Return: the result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
