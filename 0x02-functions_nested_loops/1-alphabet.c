d106b3d553a17d51fddcf43c9df26f76d805a771
onyxvail <ons.hfi@gmail.com>
2023-02-21 11:46:04 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Entry point
+ *main - prints the alphabet in lowercase
+ * followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
