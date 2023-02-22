822775ed418fe760a99ae2af965d7a63579e5225
onyxvail <ons.hfi@gmail.com>
2023-02-22 05:09:43 -0800

    Task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test if the function positive or negative
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+**0x03. C - Debugging**
+May the Rubber ducks be ever in our favour
