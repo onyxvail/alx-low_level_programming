93b80d48217794d101be6134c8aab8662fbc4275
onyxvail <ons.hfi@gmail.com>
2023-03-01 14:23:34 +0100

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @src: the source of the strings
+ * @dest: The destination of the string
+ * @n: the length ofthe  int
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
