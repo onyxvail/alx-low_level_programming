609dfeed4fff61af2e920de40c8e0b94c1db0ddc
onyxvail <ons.hfi@gmail.com>
2023-03-22 13:25:27 +0000

    Task0 Fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name -function that prints a name
+ * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
+#ifndef FUNC_H
+#define FUNC_H
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
