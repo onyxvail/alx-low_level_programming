bedc9c5381f504bf57613cdd169cbc2e3a766517
onyxvail <ons.hfi@gmail.com>
2023-06-22 21:07:08 +0100

    JACKPOT

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/onyxvail/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
-export LD_PRELOAD=/tmp/preload.so
+wget -P /tmp https://github.com/onyxvail/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/tmp/libdynamic.so
