0180f120dc22bc1e47abbfb4867fe95a920a1bc3
onyxvail <ons.hfi@gmail.com>
2023-10-10 12:25:51 +0100

    task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_ALGOS_H
+#define SORTING_ALGOS_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+#endif /* SORTING_ALGOS_H */
